adf74cf8e6ba0f0c7c847d2e71f2eb4fa83a681a
Mathias Holmgaard <maho54059@edu.ucl.dk>
2024-04-29 18:17:34 +0200

    Update Call Works

--- HundeRally.WebAPI/Controllers/ObstacleController.cs
+++ HundeRally.WebAPI/Controllers/ObstacleController.cs
@@ -18,6 +18,7 @@ namespace HundeRally.WebAPI.Controllers
             _context = context;
         }
 
+        //CRUD READ
         //Denne kommando henter en liste med alle obstacles fra databasen
         [HttpGet]
         public async Task<ActionResult<List<Obstacle>>> GetAllObstacles()
@@ -27,11 +28,12 @@ namespace HundeRally.WebAPI.Controllers
 
         }
 
+        //CRUD READ BY ID
         //Denne kommando henter en obstacle fra databasen baseret på Id
         [HttpGet ]
         // [Route attributten nedenfor tillader klienten at få en bestemt obstacle baseret på Id]
         [Route ("{Id}")]
-        public async Task<ActionResult<List<Obstacle>>> GetObstacle(int Id)
+        public async Task<ActionResult<Obstacle>> GetObstacle(int Id)
         {
             var obstacle = await _context.Obstacles.FindAsync(Id);
             if (obstacle is null)
@@ -39,6 +41,39 @@ namespace HundeRally.WebAPI.Controllers
             return Ok(obstacle);
 
         }
+
+
+        //CRUD CREATE
+        [HttpPost]
+        //Her bør vi lave et DTO objekt, som vi kan bruge til at sende data til databasen. Dette gør vi for at begrænse data , som vi sender til databasen.(Altså udelader Id eksempelvis)
+        public async Task<ActionResult<List<Obstacle>>> AddObstacle(Obstacle obstacle)
+        {
+            _context.Obstacles.Add(obstacle);
+            await _context.SaveChangesAsync();
+
+            return Ok(obstacle);
+
+        }
+
+        //CRUD UPDATE
+        [HttpPut]
+        public async Task<ActionResult<List<Obstacle>>> UpdateObstacle(Obstacle updatedObstacle)
+        {
+            var dbObstacle = await _context.Obstacles.FindAsync(updatedObstacle.Id);
+            if (dbObstacle is null)
+                return NotFound("Obstacle not found");
+
+            dbObstacle.Number = updatedObstacle.Number;
+            dbObstacle.Name = updatedObstacle.Name;
+            dbObstacle.Description = updatedObstacle.Description;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(updatedObstacle);
+
+        }
+
+
     }
 }
 
