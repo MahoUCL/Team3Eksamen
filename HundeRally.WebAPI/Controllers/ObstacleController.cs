c78968d9ce81285c11cdfb9c4f3bc45d6a67de5b
Mathias Holmgaard <maho54059@edu.ucl.dk>
2024-04-29 17:49:38 +0200

    Database access through API Endpoint ready

---
+++ HundeRally.WebAPI/Controllers/ObstacleController.cs
@@ -0,0 +1,31 @@
+﻿using HundeRally.WebAPI.Data;
+using HundeRally.WebAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace HundeRally.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObstacleController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ObstacleController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Obstacle>>> GetAllObstacles()
+        {
+            var obstacles = await _context.Obstacles.ToListAsync();
+            return Ok(obstacles);
+
+        }
+    }
+}
+
+
---
+++ HundeRally.WebAPI/Entities/Obstacle.cs
@@ -0,0 +1,10 @@
+﻿namespace HundeRally.WebAPI.Entities
+{
+    public class Obstacle
+    {
+        public int Id { get; set; }
+        public int? Number { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; } = string.Empty;
+    }
+}
