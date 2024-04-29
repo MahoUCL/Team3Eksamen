9ad667cc8de4dc3c13c1bb01ba05822c189bdb51
Mathias Holmgaard <maho54059@edu.ucl.dk>
2024-04-29 17:58:14 +0200

    GetById for Obstacles

--- HundeRally.WebAPI/Controllers/ObstacleController.cs
+++ HundeRally.WebAPI/Controllers/ObstacleController.cs
@@ -18,11 +18,25 @@ namespace HundeRally.WebAPI.Controllers
             _context = context;
         }
 
+        //Denne kommando henter en liste med alle obstacles fra databasen
         [HttpGet]
         public async Task<ActionResult<List<Obstacle>>> GetAllObstacles()
         {
             var obstacles = await _context.Obstacles.ToListAsync();
             return Ok(obstacles);
+
+        }
+
+        //Denne kommando henter en obstacle fra databasen baseret på Id
+        [HttpGet ]
+        // [Route attributten nedenfor tillader klienten at få en bestemt obstacle baseret på Id]
+        [Route ("{Id}")]
+        public async Task<ActionResult<List<Obstacle>>> GetObstacle(int Id)
+        {
+            var obstacle = await _context.Obstacles.FindAsync(Id);
+            if (obstacle is null)
+                return NotFound("Obstacle not found");
+            return Ok(obstacle);
 
         }
     }
